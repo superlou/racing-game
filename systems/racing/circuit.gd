76f6ba91f6051909a71edb8b10fe832a1261847e
superlou <lousimons@gmail.com>
2025-09-03 21:37:10 -0400

    Checkpoint tracking

--- systems/racing/circuit.gd
+++ systems/racing/circuit.gd
@@ -1,8 +1,22 @@
-extends Node
+extends Resource
 class_name Circuit
 
-@export var checkpoints := []
+@export var sequence:Dictionary[String, String]
 
-var next_map:Dictionary = {
-	$SimpleGate: [$SimpleGate2]
-}
+
+func checkpoints_connect(from:String, to:String) -> bool:
+    var checkpoint_connections := sequence[from]
+
+    if checkpoint_connections == "":
+        var index := sequence.keys().find(from)
+        var next_index = (index + 1) % len(sequence)
+        var next_key = sequence.keys()[next_index]
+        return next_key == to
+
+    # todo Handle specified checkpoints or multiple connections
+
+    return false
+
+
+func is_first_checkpoint(from:String) -> bool:
+    return sequence.keys()[0] == from
