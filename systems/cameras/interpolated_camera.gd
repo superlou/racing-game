565e036742494ce799d103a0e1dff80cdc4ec341
superlou <lousimons@gmail.com>
2026-04-12 14:21:28 -0400

    Working towards better camera

--- systems/cameras/interpolated_camera.gd
+++ systems/cameras/interpolated_camera.gd
@@ -3,23 +3,38 @@ extends Camera3D
 class_name InterpolatedCamera
 
 @export var lerp_speed = 5.0
-@export var target:Node3D
+@export var target:RigidBody3D
 @export var offset := Vector3.ZERO
-@export var look_at_target:Node3D
 @export var look_at_offset := Vector3.ZERO
 
+var was_moving := false
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if !target:
 		return
 
-	var target_transform = target.global_transform.translated_local(offset)
+	var up = target.transform.basis.y
+	var target_velocity = target.linear_velocity
+
+	var target_transform = global_transform
+
+	print(was_moving)
+
+	if target.linear_velocity.length() > 2.0:
+		was_moving = true
+
+	if was_moving:
+		var norm_velocity := target.linear_velocity.normalized()
+		target_transform = target.global_transform \
+			.translated(-norm_velocity * offset.z) \
+			.translated(up * offset.y)
+	else:
+		target_transform = target.global_transform.translated_local(offset)
+
 	global_transform = global_transform.interpolate_with(
 		target_transform,
 		lerp_speed * get_process_delta_time()
 	)
 
-	if look_at_target:
-		look_at(look_at_target.global_transform.origin + target.global_basis * look_at_offset, look_at_target.transform.basis.y)
-	else:
-		look_at(target.global_transform.origin, target.transform.basis.y)
+	var look_at_target = target.global_transform.origin + target.global_basis * look_at_offset
+	look_at(look_at_target, up)
